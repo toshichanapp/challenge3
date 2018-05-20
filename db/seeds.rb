2c6352ab013af3c69027ce93486448c7b27fe2d9
toshichanapp <toshichan@synapz.jp>
2018-05-20 22:49:56 +0900

    import house_data.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'csv'
+
+CSV.foreach('db/house_data.csv', headers: true) do |row|
+  has_child = row['has_child'] == 'Yes'
+  House.create(firstname: row['Firstname'],
+               lastname: row['Lastname'],
+               city: row['city'],
+               num_of_people: row['num_of_people'],
+               has_child: has_child
+              )
+end

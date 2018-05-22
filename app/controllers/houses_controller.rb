d655d951564d63cba687ebbfac76773b2e56ec3f
toshichanapp <toshichan@synapz.jp>
2018-05-23 00:13:34 +0900

    city別人口　棒グラフ表示

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,14 @@
 class HousesController < ApplicationController
+  
+  def index
+    houses = House.all
+    cities = houses.pluck(:city).uniq
+    @population = {}
+    @has_child = {}
+    cities.each do |city|
+      @population[city] = houses.where(city: city).pluck(:num_of_people).reduce(&:+)
+      @has_child[city] = houses.where(city: city, has_child: true).length
+    end
+    
+  end
 end

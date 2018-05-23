513152d1c70386b86bf240ffd4bc99b4b29c566e
Toshiki Fujiyoshi <toshichan@synapz.jp>
2018-05-23 10:37:25 +0900

    子供を持っている人と持っていない人の割合を円グラフで表示

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,13 +2,15 @@ class HousesController < ApplicationController
   
   def index
     houses = House.all
+
     cities = houses.pluck(:city).uniq
     @population = {}
-    @has_child = {}
     cities.each do |city|
       @population[city] = houses.where(city: city).pluck(:num_of_people).reduce(&:+)
-      @has_child[city] = houses.where(city: city, has_child: true).length
     end
-    
+
+    @has_child = {}
+    @has_child['has'] = houses.where(has_child: true).length
+    @has_child['none'] = houses.where(has_child: false).length
   end
 end
